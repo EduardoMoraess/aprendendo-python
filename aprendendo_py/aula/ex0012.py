f6cf13bb0c26a8ea04b2e08c526f3585dd72f3cb
EduardoMoraess <eduzinho2118@gmail.com>
2026-09-20 20:35:11 -0300

    aula exercecio 12 finalizada

--- aprendendo_py/aula/ex0012.py
+++ aprendendo_py/aula/ex0012.py
@@ -11,3 +11,4 @@ print('Seu nome em maiusculas é {}'.format(nome_usuario.upper()))
 print('Seu nome em minuscula é {}'.format(nome_usuario.lower()))
 
 print('Seu nome ao todo tem {} letras'.format(len(nome_usuario)-nome_usuario.count(' ')))
+print('Seu primeiro nome tem {} letras'.format(nome_usuario.find(' ')))
