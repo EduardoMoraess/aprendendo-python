65c2210c7af52436c6d27bfe0e8a37de105333cf
EduardoMoraess <eduzinho2118@gmail.com>
2026-09-19 21:28:21 -0300

    # Analisador de Textos

---
+++ aprendendo_py/aula/ex0012.py
@@ -0,0 +1,13 @@
+# Analisador de Textos
+
+from time import sleep
+
+nome_usuario = str(input('Digte seu nome completo:=>')).strip()
+
+print('Analisando seu nome....')
+sleep(3)
+
+print('Seu nome em maiusculas é {}'.format(nome_usuario.upper()))
+print('Seu nome em minuscula é {}'.format(nome_usuario.lower()))
+
+print('Seu nome ao todo tem {} letras'.format(len(nome_usuario)-nome_usuario.count(' ')))
