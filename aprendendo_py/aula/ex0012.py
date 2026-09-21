d75f07f5520d05f37f54448f5b3d30d44c394cb0
EduardoMoraess <eduzinho2118@gmail.com>
2026-09-20 21:57:17 -0300

    forma simples de entender o codigo

--- aprendendo_py/aula/ex0012.py
+++ aprendendo_py/aula/ex0012.py
@@ -11,4 +11,9 @@ print('Seu nome em maiusculas é {}'.format(nome_usuario.upper()))
 print('Seu nome em minuscula é {}'.format(nome_usuario.lower()))
 
 print('Seu nome ao todo tem {} letras'.format(len(nome_usuario)-nome_usuario.count(' ')))
-print('Seu primeiro nome tem {} letras'.format(nome_usuario.find(' ')))
+#print('Seu primeiro nome tem {} letras'.format(nome_usuario.find(' ')))
+
+separa = nome_usuario.split()
+
+#comando mais simples de se entender apesar de grande mas esta mais explicativo
+print('Seu nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
