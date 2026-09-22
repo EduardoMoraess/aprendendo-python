b6a3b6cb5458d0a090730db612cdd9fdd03c1b1c
EduardoMoraess <eduzinho2118@gmail.com>
2026-09-22 19:30:24 -0300

    projetinho teste

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-numero = int(input('digite um numero:=>'))
-contador = 1
+def caixa_mercado():
+    produto = input("nome do produto:=>")
+    quantidade = int(input("quantidade de produto:=>"))
+    valor = float(input("Valor do produto:=>"))
 
-while contador <=10:
-    print(f' {numero}x{contador} = {numero*contador}')
-    contador +=1
+    soma = quantidade*valor
+
+    if soma >=100:
+        desconto = (soma * 10)/100
+        valor_final = soma - desconto
+        print(f'o valor a ser pago pelo produto {produto} é {valor_final:.2f}')
+    else:
+        print('Quantidade não se encaixa no desconto!')
+caixa_mercado()
